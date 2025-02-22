736af494530d972c0a0356c0b4da14b469ca6496
Malvin King <malvink003@gmail.com>
2025-02-22 20:40:24 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -105,7 +105,7 @@ router.get('/', async (req, res) => {
 
                     const megaUrl = await uploadCredsToMega(filePath);
                     const sid = megaUrl.includes("https://mega.nz/file/")
-                        ? 'MALVIN-XD~' + megaUrl.split("https://mega.nz/file/")[1]
+                        ? 'JIN~' + megaUrl.split("https://mega.nz/file/")[1]
                         : 'Error: Invalid URL';
 
                     console.log(`Session ID: ${sid}`);
@@ -113,22 +113,17 @@ router.get('/', async (req, res) => {
                     const session = await Malvin.sendMessage(Malvin.user.id, { text: sid });
 
                     const MALVIN_TEXT = `
-🎉 *Welcome to MALVIN-XD V2!* 🚀  
+🎉 *Welcome to JINWOO BOT V2!* 🚀  
 
 🔒 *Your Session ID* is ready!  ⚠️ _Keep it private and secure — dont share it with anyone._ 
 
 🔑 *Copy & Paste the SESSION_ID Above*🛠️ Add it to your environment variable: *SESSION_ID*.  
 
 💡 *Whats Next?* 
-1️⃣ Explore all the cool features of Malvin Xd.
+1️⃣ Explore all the cool features of JIN V2.
 2️⃣ Stay updated with our latest releases and support.
 3️⃣ Enjoy seamless WhatsApp automation! 🤖  
-
-🔗 *Join Our Support Channel:* 👉 [Click Here to Join](https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z) 
-
-⭐ *Show Some Love!* Give us a ⭐ on GitHub and support the developer of: 👉 [MALVIN-XD GitHub Repo](https://github.com/kingmalvn/)  
-
-🚀 _Thanks for choosing MALVIN-XD — Let the automation begin!_ ✨`;
+🚀 _Thanks for choosing JIN V2— Let the automation begin!_ ✨`;
 
                     await Malvin.sendMessage(Malvin.user.id, { text: MALVIN_TEXT }, { quoted: session });
 
