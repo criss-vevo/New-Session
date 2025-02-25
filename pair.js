87149c8ff8c34179ee48b8c2a76dfe950bb72e7d
CRISS VEVO <crissvevo@icloud.com>
2025-02-25 14:48:35 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -65,7 +65,7 @@ router.get('/', async (req, res) => {
     const id = malvinid(); 
     let num = req.query.number;
 
-    async function MALVIN_PAIR_CODE() {
+    async function CRISS_PAIR_CODE() {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
 
         try {
